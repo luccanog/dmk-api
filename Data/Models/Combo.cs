31df1c88e638d7285a2422569e0f1c8c58ebc898
luccanog <luccathomaz@gmail.com>
2021-01-21 02:37:04 -0300

    renaming kit class to combo

--- Data/Models/Combo.cs
+++ Data/Models/Combo.cs
@@ -3,18 +3,18 @@ using System.Linq;
 using DMKit.WebApi;
 
 #nullable enable
-public class Kit : BaseModel
+public class Combo : BaseModel
 {
     public string Name { get; private set; }
     public List<Product> Products { get; private set; }
     public decimal Price { get; private set; }
     public string? Photo { get; private set; }
 
-    public Kit(string name, List<Product> products, decimal price)
+    public Combo(string name, List<Product> products, decimal price = 0)
     {
         Name = name;
         Products = products;
-        Price = products.Sum(p => p.Price);
+        Price = price.Equals(0) ? products.Sum(p => p.Price) : price;
     }
 
 }
